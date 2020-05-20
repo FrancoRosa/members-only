3e59427324638b349f7c11c781e5b40426cbb7a6
FrancoRosa <km115.franco@gmail.com>
2020-05-20 18:59:18 -0500

    Add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts
 
+  validates :name, presence: true, length: { in: 3..16 }
+  validates :member_status, presence: true, length: { in: 3..16 }
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 end
