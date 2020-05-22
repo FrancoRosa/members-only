4b066cacca4512602dabfc0edea3e06d195e8f43
FrancoRosa <km115.franco@gmail.com>
2020-05-22 09:54:11 -0500

    Fix cascade error due to user destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :posts
+  has_many :posts, dependent: :destroy
 
   validates :name, presence: true, length: { in: 3..16 }
   validates :member_status, presence: true, length: { in: 3..16 }
